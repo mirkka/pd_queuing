e3e3881d2279b5140cfc7b61d2fe0093839a9f64
Mirka Lison <mirka.lison@gmail.com>
2018-09-20 14:36:35 +0300

    success email class info, logging, error handling

--- main.js
+++ main.js
@@ -12,11 +12,25 @@ const cliParams = cli.parse({
 const password = cliParams.password;
 const email = cliParams.email;
 
+let awsCredentials = {};
+let awsRegion;
+
+try {
+  const parsedCredentials = JSON.parse(cliParams.credentials);
+  awsCredentials.email = parsedCredentials.email;
+  awsCredentials.password = parsedCredentials.password;
+  awsRegion = parsedCredentials.region;
+} catch (error) {
+  console.log(new Date(), 'ERROR', error);
+  return;
+}
+
 AWS.config.credentials = JSON.parse(cliParams.credentials);
+AWS.config.region = JSON.parse(cliParams.credentials).region;
 
 const ses = new AWS.SES();
 
-const emailParams = {
+let emailParams = {
   Destination: {
     ToAddresses: [
       'mirka.lison@gmail.com'
@@ -25,11 +39,11 @@ const emailParams = {
   Message: {
     Body: {
       Html: {
-        Data: 'Booking successfull',
+        Data: '',
         Charset: "UTF-8",
       },
       Text: {
-        Data: 'Booking successfull',
+        Data: `Subscription successfull! Booked at ${new Date()}`,
         Charset: "UTF-8",
       }
     },
@@ -55,16 +69,18 @@ const emailParams = {
   await page.waitForSelector("[class=CALENDARCONT]");
   console.log('login success');
 
-  const handleButton = async (button) => {
-    await page.screenshot({path: 'example.png', fullPage: true});
+  const handleButton = async (button, classDetails) => {
+    await page.screenshot({path: 'screenshot.png', fullPage: true});
     await button.click();
-    console.log('subscription success!');
+    console.log(`Subscription successfull! Booked at ${new Date()}`);
+
+    emailParams.Message.Body.Html.Data = `Subscription successfull! <br /> Class details: ${classDetails} <br />Booked at ${new Date()}`
 
     ses.sendEmail(emailParams, (err, data) => {
       if (err) {
-        console.log(err, err.stack);
+        console.log('ERROR',err, err.stack);
       } else {
-        console.log('email sent');
+        console.log(new Date(), 'email sent success');
       }
     });
 
@@ -75,32 +91,35 @@ const emailParams = {
     try {
       await page.waitForSelector('.glyphicon-plus', {timeout: 10 * 1000});
     } catch (error) {
-      console.log('no queued class found');
+      console.log(new Date(), 'no queued class found');
       await browser.close();
       return;
     }
 
     //for testing only - creates fake queue button
-    // if(index === 1) {
+    // change class ID in href selector
+    // if(index === 0) {
     //   await page.evaluate(() => {
-    //     let elem = document.querySelector('a[href*="classid=26306"][title="Book class"]');
+    //     let elem = document.querySelector('a[href*="classid=26328"][title="Book class"]');
     //     if(!elem) { return; }
     //     elem.classList.add('btn-warning');
     //   });
     // }
 
     const button = await page.$('.btn-warning');
+
     if(!button) {
       const forwardArrow = await page.$('.glyphicon-chevron-right');
 
       await forwardArrow.click();
       await page.waitForNavigation();
 
-      console.log('next page');
+      console.log('next page', index+1);
 
       checkPage(index+1);
     } else {
-      handleButton(button);
+      const classDetails = await page.evaluateHandle(() => document.querySelector('.btn-warning').parentElement.querySelector('.classbutton').innerHTML);
+      handleButton(button, classDetails);
     }
   }
   checkPage(0);
