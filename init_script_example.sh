c5c4442bf5e8c0fc85704a2a2530fcfe895b9252
Mirka Lison <mirka.lison@gmail.com>
2018-10-26 11:52:47 +0300

    translated init script example comment to english

--- init_script_example.sh
+++ init_script_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# dependencie
+# dependencies
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install --yes nodejs build-essential gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
@@ -11,10 +11,10 @@ git clone https://github.com/mirkka/pd_queuing.git
 # change ownership of the directory to user ubuntu
 chown -R ubuntu:ubuntu /pd_queuing
 
-# movni to co sme naklonovali do home folderu usera ubuntu
+# move cloned repo to ubuntu user home folder
 mv /pd_queuing /home/ubuntu/
 
-# runni npm ako user ubuntu, v specifickej directory
+# runn npm as user ubuntu, in a specific directory
 sudo -H -u ubuntu bash -c '/usr/bin/npm --prefix /home/ubuntu/pd_queuing install /home/ubuntu/pd_queuing'
 
 # add cron job as ubuntu user and output into log
